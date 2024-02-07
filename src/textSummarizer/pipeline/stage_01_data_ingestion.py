29106a0e70adec84a8eacd58521f1ed2d4c2c4bb
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-02-07 19:46:19 +0100

    data validation added

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,6 @@
 from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.components.data_ingestion import DataIngestion
-from textSummarizer.logging import logger
+# from textSummarizer.logging import logger
 
 
 class DataIngestionTrainingPipeline:
